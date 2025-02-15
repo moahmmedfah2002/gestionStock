afe160d47067df54876c8da4a47bc7c9fc8f66ed
pc <fahlaouimohammed@gmail.com>
2025-02-15 20:09:28 +0100

    project

---
+++ src/main/java/ma/ensa/gestiondestockclube/GestionDeStockClubeApplication.java
@@ -0,0 +1,13 @@
+package ma.ensa.gestiondestockclube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeStockClubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDeStockClubeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/ensa/gestiondestockclube/GestionDeStockClubeApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensa.gestiondestockclube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeStockClubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
